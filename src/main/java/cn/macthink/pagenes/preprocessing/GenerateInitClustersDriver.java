71cc9d88a0d41fabf5b201875d71ae321e9c0db4
macthink <macthink@outlook.com>
2013-06-30 09:49:22 +0800

    初始化提交

---
+++ src/main/java/cn/macthink/pagenes/preprocessing/GenerateInitClustersDriver.java
@@ -0,0 +1,25 @@
+/**
+ * Project:cn.macthink.pagenes
+ * File Created at 2013-6-28
+ * Auther:Macthink
+ * 
+ * Copyright 2013 Macthink.cn.
+ * All rights reserved.
+ */
+package cn.macthink.pagenes.preprocessing;
+
+/**
+ * GenerateInitClustersDriver
+ * 
+ * @author Macthink
+ */
+public class GenerateInitClustersDriver {
+
+	/**
+	 * 
+	 */
+	public GenerateInitClustersDriver() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
